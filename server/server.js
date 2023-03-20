aa9a0b0c4610abe9c338994c628b33dd6dae84ed
Fernando castro <fernandocastro77@yahoo.com>
2023-03-20 12:46:11 -0400

    making all the user routes

--- server/server.js
+++ server/server.js
@@ -1,21 +1,6 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-// const express = require('express');
-// const cors = require('cors');
-// const { client } = require('pg');
 
-// const config = require('./config')[process.env.NODE_ENV || 'production'];
-// const PORT = config.port;
-
-// const app = express();
-
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Example app listening on port ${PORT}`);
-// });
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
@@ -23,8 +8,8 @@ const app = express();
 const port = 3000;
 const pool = new Pool({
     user: 'postgres',
-    host: 'postgres',
-    database: 'cashCoach',
+    host: '127.0.0.1',
+    database: 'cashcoach',
     password: 'docker',
     port: 5432,
 
@@ -32,10 +17,51 @@ const pool = new Pool({
 app.use(cors());
 app.use(express.json());
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/user', (req, res)=>{
+    async function getUser(){
+        try{
+            const result = await pool.query('SELECT * FROM users');
+            res.status(200).send(result.rows);
+        }catch(e){
+            console.log(e.stack);
+        }
+    }
+    getUser();
 });
+  
+
+app.get('/user/:id', async (req, res) => {
+    async function getUserById(){
+        try{
+            const result = await pool.query(`SELECT * FROM users WHERE id = ${req.params.id}`);
+            if(result.rows.length === 0){
+                res.sendStatus(404);
+            }else{
+                res.status(200).send(result.rows[0]);
+            }
+        }catch(e){
+            console.log(e.stack);
+        }
+    }
+    getUserById();
+});
+
+    
+app.post('/user', async (req, res) => {
+    async function createUser(){
+        try{
+            let user = req.body;
+            let name = user.name;
+            let email = user.email;
+            const result = await pool.query(`INSERT INTO users (name, email) VALUES ('${name}', '${email}') RETURNING *`);
+            res.status(201).send(result.rows[0]);
+        }catch(e){
+            console.e(e.stack);
+        }
+    }
+    createUser();
+});
+
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
