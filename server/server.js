59c7bfc2950f6eeabbb254f2256008fcc8f9f06a
Fernando castro <fernandocastro77@yahoo.com>
2023-03-21 15:14:52 -0400

    just finsihed routes for users and transactions

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const pool = new Pool({
 });
 app.use(cors());
 app.use(express.json());
-
+// to get all the users 
 app.get('/user', (req, res)=>{
     async function getUser(){
         try{
@@ -28,8 +28,7 @@ app.get('/user', (req, res)=>{
     }
     getUser();
 });
-  
-
+// to get a user by id
 app.get('/user/:id', async (req, res) => {
     async function getUserById(){
         try{
@@ -45,8 +44,7 @@ app.get('/user/:id', async (req, res) => {
     }
     getUserById();
 });
-
-    
+// to create a user
 app.post('/user', async (req, res) => {
     async function createUser(){
         try{
@@ -60,6 +58,130 @@ app.post('/user', async (req, res) => {
         }
     }
     createUser();
+});
+
+app.delete('/user/:id', (req, res)=>{
+    async function deleteUser(){
+        try{
+            const result = await pool.query(`Delete From users WHERE id = ${req.params.id} RETURNING *`);
+            if(result.rows.length === 0){
+                res.sendStatus(404);
+            }else{
+                res.status(200).send(result.rows[0]);
+            }
+        }catch(e){
+            console.error(e.stack);
+        }
+    }
+    deleteUser();
+});
+
+app.patch('/user/:id', (req, res)=>{
+    async function updateUser(){
+        try{
+            let user = req.body;
+            let name = user.name;
+            let email = user.email;
+            const result = await pool.query(`UPDATE users SET name = '${name}', email = '${email}' WHERE id = ${req.params.id} RETURNING *`);
+            if(result.rows.length === 0){
+                res.sendStatus(404);
+            }else{
+                res.status(200).send(result.rows[0]);
+            }
+        }catch(e){
+            console.error(e.stack);
+        }
+    }
+    updateUser();
+});
+
+// Get all transactions
+app.get('/transactions', async (req, res) => {
+    async function getTransactions() {
+        try {
+            const result = await pool.query('SELECT * FROM transactions');
+            res.status(200).send(result.rows);
+        } catch (e) {
+            console.log(e.stack);
+        }
+    }
+    getTransactions();
+});
+
+// Get a transaction by ID
+app.get('/transactions/:id', async (req, res) => {
+    async function getTransactionById() {
+        try {
+            const result = await pool.query(`SELECT * FROM transactions WHERE id = ${req.params.id}`);
+            if (result.rows.length === 0) {
+                res.sendStatus(404);
+            } else {
+                res.status(200).send(result.rows[0]);
+            }
+        } catch (e) {
+            console.log(e.stack);
+        }
+    }
+    getTransactionById();
+});
+
+// Create a transaction
+app.post('/transactions', async (req, res) => {
+    async function createTransaction() {
+        try {
+            let transaction = req.body;
+            let user_id = transaction.user_id;
+            let amount = transaction.amount;
+            let description = transaction.description;
+            let category = transaction.category;
+            let date = transaction.date;
+            const result = await pool.query(`INSERT INTO transactions (user_id, amount, description, category, date) VALUES (${user_id}, ${amount}, '${description}', '${category}', '${date}') RETURNING *`);
+            res.status(201).send(result.rows[0]);
+        } catch (e) {
+            console.error(e.stack);
+        }
+    }
+    createTransaction();
+});
+
+// Delete a transaction by ID
+app.delete('/transactions/:id', async (req, res) => {
+    async function deleteTransaction() {
+        try {
+            const result = await pool.query(`DELETE FROM transactions WHERE id = ${req.params.id} RETURNING *`);
+            if (result.rows.length === 0) {
+                res.sendStatus(404);
+            } else {
+                res.status(200).send(result.rows[0]);
+            }
+        } catch (e) {
+            console.error(e.stack);
+        }
+    }
+    deleteTransaction();
+});
+
+// Update a transaction by ID
+app.patch('/transactions/:id', async (req, res) => {
+    async function updateTransaction() {
+        try {
+            let transaction = req.body;
+            let user_id = transaction.user_id;
+            let amount = transaction.amount;
+            let description = transaction.description;
+            let category = transaction.category;
+            let date = transaction.date;
+            const result = await pool.query(`UPDATE transactions SET user_id = ${user_id}, amount = ${amount}, description = '${description}', category = '${category}', date = '${date}' WHERE id = ${req.params.id} RETURNING *`);
+            if (result.rows.length === 0) {
+                res.sendStatus(404);
+            } else {
+                res.status(200).send(result.rows[0]);
+            }
+        } catch (e) {
+            console.error(e.stack);
+        }
+    }
+    updateTransaction();
 });
 
 
